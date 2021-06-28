ae1e8265b77345fa4e36a2d667bd86fcd990f941
Matthew Bouc <boucgm@gmail.com>
2021-06-28 13:13:29 -0500

    BSetting up POST/GET requests between client/server.  Client POST complete.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,3 +5,23 @@ $( document ).ready( onReady );
 function onReady() {
     console.log('DOM ready');
 }
+
+
+function serverPOST(){
+    $.ajax({
+        method: 'POST',
+        url: '/jokeArray',
+        data: {
+            author: $('#whoseJokeIn').val(),
+            question: $('#questionIn').val(),
+            punch: $('#punchlineIn').val()
+        }
+    })
+    .then(function(response){
+        console.log('joke sent to server', response);
+        ///UPDATE WITH GET REQUEST HERE
+    })
+    .catch(function(err){
+        console.log('err', err);
+    })
+}
--- server/server.js
+++ server/server.js
@@ -5,6 +5,16 @@ const PORT = 5000;
 
 // use bodyParser.urlencoded throughout the app with this:
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// serve back static files
+app.use(express.static('server/public'));
+
+app.listen(PORT, () => {
+  console.log('server running on: ', PORT);
+}); // end spin up server
+
 
 let jokes = [
   {
@@ -34,9 +44,3 @@ let jokes = [
   }
 ];
 
-// serve back static files
-app.use(express.static('server/public'));
-
-app.listen(PORT, () => {
-  console.log('server running on: ', PORT);
-}); // end spin up server
